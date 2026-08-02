e5ede2141610ee86b4aed1964b4b035fcb3231af
sreenivas-anekal <sreenivasaanekal@gmail.com>
2026-08-02 09:47:09 +0530

    Create vehicle_state.h

---
+++ app/inc/vehicle_state.h
@@ -0,0 +1,39 @@
+#ifndef VEHICLE_STATE_H
+#define VEHICLE_STATE_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Define all possible operating states of the vehicle ECU */
+typedef enum {
+    STATE_OFF = 0,
+    STATE_IGNITION,
+    STATE_IDLE,
+    STATE_RUNNING,
+    STATE_FAULT
+} VehicleState_t;
+
+/* Define events that trigger state transitions */
+typedef enum {
+    EVENT_IGNITION_ON = 0,
+    EVENT_SELF_TEST_OK,
+    EVENT_ENGINE_START,
+    EVENT_VEHICLE_MOVE,
+    EVENT_CRITICAL_FAULT,
+    EVENT_FAULT_CLEARED,
+    EVENT_SHUTDOWN
+} VehicleEvent_t;
+
+void VehicleState_Init(void);
+VehicleState_t VehicleState_ProcessEvent(VehicleEvent_t event);
+VehicleState_t VehicleState_GetCurrent(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VEHICLE_STATE_H */

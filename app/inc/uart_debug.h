ea8e35aaa351c16ef96c3f47918e6bda8b5143a0
sreenivas-anekal <sreenivasaanekal@gmail.com>
2026-08-02 12:39:32 +0530

    Create uart_debug.h

---
+++ app/inc/uart_debug.h
@@ -0,0 +1,23 @@
+#ifndef UART_DEBUG_H
+#define UART_DEBUG_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    LOG_LEVEL_INFO = 0,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_ERROR
+} LogLevel_t;
+
+void UART_Debug_Init(void);
+void UART_Log(LogLevel_t level, const char* tag, const char* message);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UART_DEBUG_H */

9f4e06883dc175383415676f662ef9a2d3e814cd
sreenivas-anekal <sreenivasaanekal@gmail.com>
2026-08-12 20:38:42 +0530

    Update task_scheduler.h

--- app/inc/task_scheduler.h
+++ app/inc/task_scheduler.h
@@ -10,15 +10,24 @@ extern "C" {
 
 typedef void (*TaskCallback_t)(void);
 
-typedef struct {
-    uint32_t interval_ms;     /* How often the task should run */
-    uint32_t last_run_ms;     /* Timestamp of the last execution */
-    TaskCallback_t callback;  /* Function pointer to the task */
+typedef struct
+{
+    uint32_t interval_ms;
+    uint32_t last_run_ms;
+    TaskCallback_t callback;
 } Task_t;
 
 void Task_Scheduler_Init(void);
-void Task_Register(uint8_t task_id, uint32_t interval_ms, TaskCallback_t callback);
-void Task_Scheduler_Run(uint32_t current_time_ms);
+
+void Task_Register(
+    uint8_t task_id,
+    uint32_t interval_ms,
+    TaskCallback_t callback
+);
+
+void Task_Scheduler_Run(
+    uint32_t current_time_ms
+);
 
 #ifdef __cplusplus
 }

4ece4eb82c0f5a319b7510c1426df5fb68a74ae7
sreenivas-anekal <sreenivasaanekal@gmail.com>
2026-08-12 15:48:31 +0530

    Update adc_sensor.h

--- app/inc/adc_sensor.h
+++ app/inc/adc_sensor.h
@@ -7,9 +7,15 @@
 extern "C" {
 #endif
 
+#define ADC_SENSOR_MAX_VALUE      4095U
+#define ADC_SENSOR_REFERENCE_VOLT 3.3f
+
 void ADC_Sensor_Init(void);
+
 uint16_t ADC_Read_Raw(uint8_t channel);
+
 float ADC_ConvertToVoltage(uint16_t raw_value);
+
 float ADC_GetFuelLevelPercentage(uint16_t raw_value);
 
 #ifdef __cplusplus

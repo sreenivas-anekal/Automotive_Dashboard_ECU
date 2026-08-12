26a2ef75b32e68d83c9451942d6c05a72b020b9a
sreenivas-anekal <sreenivasaanekal@gmail.com>
2026-08-12 16:39:39 +0530

    Update can_driver.h

--- app/inc/can_driver.h
+++ app/inc/can_driver.h
@@ -4,23 +4,19 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#ifdef __cplusplus
-extern "C" {
+#define CAN_MAX_DATA_LENGTH 8U
+
+typedef struct
+{
+    uint32_t id;
+    uint8_t data[CAN_MAX_DATA_LENGTH];
+    uint8_t length;
+} CAN_Message_t;
+
+void CAN_Driver_Init(void);
+
+bool CAN_SendMessage(const CAN_Message_t *message);
+
+bool CAN_ReceiveMessage(CAN_Message_t *message);
+
 #endif
-
-/* Standard CAN Frame structure */
-typedef struct {
-    uint32_t id;          /* CAN Identifier (e.g., 0x100 for Vehicle Status) */
-    uint8_t dlc;          /* Data Length Code (number of bytes, 0 to 8) */
-    uint8_t data[8];      /* Payload bytes */
-} CanMessage_t;
-
-void CAN_Driver_Init(uint32_t baudrate);
-bool CAN_Transmit(const CanMessage_t* msg);
-bool CAN_Receive(CanMessage_t* msg);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* CAN_DRIVER_H */

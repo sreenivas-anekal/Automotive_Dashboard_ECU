66472deffa740cfd67aea7ae066ef895274249cd
sreenivas-anekal <sreenivasaanekal@gmail.com>
2026-08-12 19:59:53 +0530

    Update vehicle_state.c

--- wokwi/vehicle_state.c
+++ wokwi/vehicle_state.c
@@ -1,33 +1,159 @@
+#include "main.h"
 #include "vehicle_state.h"
-#include <stdio.h>
+
+#include <string.h>
+
+UART_HandleTypeDef huart2;
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+
+static void UART_Print(const char *message);
 
 int main(void)
 {
-    printf("\n");
-    printf("========================================\n");
-    printf(" Automotive Dashboard ECU\n");
-    printf(" Vehicle State Machine\n");
-    printf(" STM32C031C6\n");
-    printf("========================================\n");
+    HAL_Init();
+
+    SystemClock_Config();
+    MX_GPIO_Init();
+    MX_USART2_UART_Init();
+
+    UART_Print("\r\n");
+    UART_Print("========================================\r\n");
+    UART_Print(" Automotive Dashboard ECU\r\n");
+    UART_Print(" Vehicle State Machine\r\n");
+    UART_Print(" STM32C031C6\r\n");
+    UART_Print("========================================\r\n");
 
     VehicleState_Init();
 
-    printf("Initial state: OFF\n");
+    UART_Print("Initial state: OFF\r\n");
 
     VehicleState_ProcessEvent(EVENT_IGNITION_ON);
-    printf("Event: IGNITION_ON  -> State: IGNITION\n");
+
+    UART_Print(
+        "Event: IGNITION_ON  -> State: IGNITION\r\n"
+    );
 
     VehicleState_ProcessEvent(EVENT_SELF_TEST_OK);
-    printf("Event: SELF_TEST_OK -> State: IDLE\n");
+
+    UART_Print(
+        "Event: SELF_TEST_OK -> State: IDLE\r\n"
+    );
 
     VehicleState_ProcessEvent(EVENT_ENGINE_START);
-    printf("Event: ENGINE_START  -> State: RUNNING\n");
+
+    UART_Print(
+        "Event: ENGINE_START  -> State: RUNNING\r\n"
+    );
 
     VehicleState_ProcessEvent(EVENT_SHUTDOWN);
-    printf("Event: SHUTDOWN      -> State: OFF\n");
 
-    printf("----------------------------------------\n");
-    printf("Vehicle state test complete.\n");
+    UART_Print(
+        "Event: SHUTDOWN      -> State: OFF\r\n"
+    );
 
-    return 0;
+    UART_Print("----------------------------------------\r\n");
+
+    while (1)
+    {
+        HAL_Delay(1000);
+    }
+}
+
+static void UART_Print(const char *message)
+{
+    HAL_UART_Transmit(
+        &huart2,
+        (uint8_t *)message,
+        strlen(message),
+        HAL_MAX_DELAY
+    );
+}
+
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef osc = {0};
+    RCC_ClkInitTypeDef clk = {0};
+
+    osc.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    osc.HSIState = RCC_HSI_ON;
+    osc.HSIDiv = RCC_HSI_DIV1;
+    osc.HSICalibrationValue =
+        RCC_HSICALIBRATION_DEFAULT;
+
+    if (HAL_RCC_OscConfig(&osc) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    clk.ClockType =
+        RCC_CLOCKTYPE_HCLK |
+        RCC_CLOCKTYPE_SYSCLK |
+        RCC_CLOCKTYPE_PCLK1;
+
+    clk.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    clk.SYSCLKDivider = RCC_SYSCLK_DIV1;
+    clk.AHBCLKDivider = RCC_HCLK_DIV1;
+    clk.APB1CLKDivider = RCC_APB1_DIV1;
+
+    if (HAL_RCC_ClockConfig(
+            &clk,
+            FLASH_LATENCY_1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+static void MX_GPIO_Init(void)
+{
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+}
+
+static void MX_USART2_UART_Init(void)
+{
+    GPIO_InitTypeDef gpio = {0};
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_USART2_CLK_ENABLE();
+
+    gpio.Pin = GPIO_PIN_2 | GPIO_PIN_3;
+    gpio.Mode = GPIO_MODE_AF_PP;
+    gpio.Pull = GPIO_NOPULL;
+    gpio.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    gpio.Alternate = GPIO_AF1_USART2;
+
+    HAL_GPIO_Init(GPIOA, &gpio);
+
+    huart2.Instance = USART2;
+
+    huart2.Init.BaudRate = 115200;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.Mode = UART_MODE_TX_RX;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+    huart2.Init.OneBitSampling =
+        UART_ONE_BIT_SAMPLE_DISABLE;
+    huart2.Init.ClockPrescaler =
+        UART_PRESCALER_DIV1;
+
+    huart2.AdvancedInit.AdvFeatureInit =
+        UART_ADVFEATURE_NO_INIT;
+
+    if (HAL_UART_Init(&huart2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void Error_Handler(void)
+{
+    __disable_irq();
+
+    while (1)
+    {
+    }
 }
